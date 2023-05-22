46734d4c32c1e5210cd51a5cbca3674039dcca98
maratVrn <begisgevmr@mail.ru>
2023-05-22 15:12:41 +0300

    ушли от куки совсем

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -19,7 +19,7 @@ class UserController{
             const userData = await userService.registration(email, password)
 
 
-            res.cookie('refreshToken', userData.refreshToken, {maxAge : 30*24*60*60*1000, httpOnly: true})
+            // res.cookie('refreshToken', userData.refreshToken, {maxAge : 30*24*60*60*1000, httpOnly: true})
             return res.json(userData)
 
         } catch (e) {
@@ -79,9 +79,9 @@ class UserController{
 
     async logout (req, res, next) {
         try {
-            const {refreshToken} = req.cookies
+            const {refreshToken} = req.body
             const token = await userService.logout(refreshToken)
-            res.clearCookie('refreshToken')
+            // res.clearCookie('refreshToken')
             return res.json(token)
 
         } catch (e) {
