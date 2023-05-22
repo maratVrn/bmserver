2e43ff070c097832b864b80ef477f2d655fe62c3
maratVrn <begisgevmr@mail.ru>
2023-05-22 11:22:32 +0300

    вернул отправку подтв письма

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -96,7 +96,7 @@ class UserController{
         try {
             const {refreshToken} = req.cookies
             console.log('куки');
-            console.log(req.cookies.refreshToken);
+            console.log(req.cookies.length);
 
             console.log('Достали refreshToken из куки');
             console.log(refreshToken);
