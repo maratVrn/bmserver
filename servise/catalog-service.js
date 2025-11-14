e7da8226acdc93d6acdff84e1013bb16a6481bc2
maratVrn <begisgevmr@mail.ru>
2025-11-14 11:23:14 +0300

    удалил лишнее

--- servise/catalog-service.js
+++ servise/catalog-service.js
@@ -2,33 +2,70 @@
 const {saveErrorLog} = require("./log");
 const {WBCatalogInfo, WBAllSubjects, WBCatalog} = require("../models/models");
 const {PARSER_SubjectsList} = require("../wbdata/wbParserFunctions");
+const request = require("request");
+const fs = require("fs");
 class CatalogService{
 
     async test (){
-        await this.updateAllSubjects_inBD()
-      return ' test ok'
 
-        // const WBCatalog_ALL = await WBCatalog.findOne({
-        //     order: [ [ 'createdAt', 'DESC' ]],
-        // })
-        // if (WBCatalog_ALL.catalogInfo) {
-        //
-        //     for (let i = 0; i < WBCatalog_ALL.catalogInfo.length; i++) {
-        //
-        //         const curId = WBCatalog_ALL.catalogInfo[i].id
-        //         if (curId === 131113) {
-        //             const subjects = await PARSER_SubjectsList(WBCatalog_ALL.catalogInfo[i])
-        //             console.log(subjects);
-        //             await WBAllSubjects.bulkCreate([{
-        //                 catalogId: curId,
-        //                 subjects: subjects,
-        //                 addFilters: []
-        //             }], {updateOnDuplicate: ["subjects", 'addFilters']}).then()
-        //
-        //         }
-        //     }
-        // }
-        // return 'updateAllSubjects_inBD isOk '
+        let fs = require('fs'),
+            request = require('request');
+        let download = function(uri, filename, callback){
+            request.head(uri, function(err, res, body){
+                request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+            });
+        };
+
+        // await this.updateAllSubjects_inBD()
+      // return ' test ok'
+
+        const WBCatalog_ALL = await WBCatalog.findOne({
+            order: [ [ 'createdAt', 'DESC' ]],
+        })
+
+        let allChildsCount = 0
+         const getImg = (child) =>{
+            allChildsCount ++
+
+            const curId = child.id
+
+            let uri = `https://static-basket-01.wbbasket.ru/vol1/wbx-catalog/promo_v2/${curId}.webp`
+
+            try {
+             download(uri, `/cat/${curId}.webp`, function(){
+
+            });
+            console.log(curId+' ok');}
+            catch (e) {console.log(curId+' -------------- ошибка -----  url : '+uri)}
+
+
+            if (child.childs)
+                if (child.childs.length > 0) for (let i in child.childs) getImg(child.childs[i])
+
+        }
+
+
+        if (WBCatalog_ALL.catalogLite) {
+
+            for (let i = 0; i < WBCatalog_ALL.catalogLite.length; i++) {
+                getImg(WBCatalog_ALL.catalogLite[i])
+
+                // const curId = WBCatalog_ALL.catalogInfo[i].id
+                //
+                // let uri = `https://static-basket-01.wbbasket.ru/vol1/wbx-catalog/promo_v2/${curId}.webp`
+                //
+                // try {
+                // await download(uri, `/cat/${curId}.webp`, function(){
+                //
+                // });
+                // console.log(curId+' ok');}
+                // catch (e) {console.log(curId+' -------------- ошибка -----  url : '+uri)}
+
+
+            }
+        }
+        console.log(allChildsCount);
+        return 'updateAllSubjects_inBD isOk '
 
     }
     // Формируем список категорий по
--- wbdata/wbfunk.js
+++ wbdata/wbfunk.js
@@ -421,7 +421,7 @@ function getPriceFromHistoryLight (history = [], dayCount = 30 ){
         const s = startDateInBase.split('.')
         crDate = new Date(s[2]+'-'+s[1]+'-'+s[0]);
         crHistory = history[needStartI]
-        AllHistory.push({d:crHistory.d, sp: crHistory.sp})
+        AllHistory.push(crHistory.sp)
     }
 
 
@@ -470,7 +470,7 @@ function calcDiscount (history = []){
     let discount2 = 0
 
     // Правильнее по медиане посчитать
-    if (priceArray.length >= dayCalc) {
+    if (priceArray.length >= dayCalc/2) {
         priceArray.sort(function (a, b) {return b - a;})
         medianPrice = priceArray[Math.round(priceArray.length / 2)]
         discount2 = Math.round(100 * (medianPrice - endPrice) / medianPrice)
